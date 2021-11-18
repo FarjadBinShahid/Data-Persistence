bd51a7ca43956e9fd412d99f3f580a2245d3a236
FarjadBinShahid <57044545+FarjadBinShahid@users.noreply.github.com>
2021-11-18 15:24:51 +0500

    added data persistence

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,6 +11,7 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text BestScoreText;
     public GameObject GameOverText;
     
     private bool m_Started = false;
@@ -22,6 +23,12 @@ public class MainManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        GameManager.Instance.LoadBestScore();
+        
+        BestScoreText.text = "BestScore: "+GameManager.Instance.playerName +" : " + GameManager.Instance.score;
+
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
@@ -70,7 +77,22 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
+        
+        if(m_Points >= GameManager.Instance.score)
+        {
+            GameManager.Instance.score = m_Points;
+            
+            GameManager.Instance.SaveBestScore();
+        }
+
         m_GameOver = true;
         GameOverText.SetActive(true);
+
     }
+
+
+    
+
+
+
 }
---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UIHandler : MonoBehaviour
+{
+    public TMP_InputField nameInputField;
+
+    public TextMeshProUGUI scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "BestScore: "+GameManager.Instance.playerName +" : " + GameManager.Instance.score;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnteredName(string name)
+    {
+        Debug.Log(nameInputField.text);
+        GameManager.Instance.playerName = nameInputField.text;
+    }
+
+    public void StartMain()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
